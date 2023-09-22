c525b12086f1c2abf1ef5b12c69c63d79e250bc4
Heech <49609924+mark-ianz@users.noreply.github.com>
2023-09-22 16:51:08 +0800

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+const sidebarElement = document.querySelector ('.sidebar-container');
+const menuElement = document.querySelector ('.hamburger-menu-container');
+const closeImage = document.querySelector('.sidebar-close-button-container');
+const sidebarModalElement = document.querySelector ('.modal-background');
+
+function toggleSidebar () {
+  if (sidebarElement.classList.contains ('sidebar-slide')) {
+    sidebarElement.classList.add ('sidebar-hide');
+    sidebarModalElement.style.display = 'none';
+  }
+  else {
+    sidebarElement.classList.add ('sidebar-slide');
+    sidebarElement.classList.remove ('sidebar-hide');
+    sidebarModalElement.style.display = 'block';
+  }
+  setTimeout (checkSidebar, 500);
+}
+
+function checkSidebar () {
+  if (sidebarElement.matches (".sidebar-slide.sidebar-hide") ) {
+    sidebarElement.classList.remove ('sidebar-slide');
+    sidebarElement.classList.remove ('sidebar-hide');
+  }
+}
