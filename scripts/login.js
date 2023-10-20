01d9504aecb0d7809039261709aaa53eb1c0852b
Heech <49609924+mark-ianz@users.noreply.github.com>
2023-10-21 00:00:19 +0800

    Add files via upload

--- scripts/login.js
+++ scripts/login.js
@@ -5,25 +5,6 @@ const usernameInput = document.querySelector ('.js-username');
 const usernameWarning = document.querySelector ('.js-username-warning')
 const passwordWarning = document.querySelector ('.js-password-warning')
 
-function login () {
-  usernameWarning.style.display = 'none';
-  passwordWarning.style.display = 'none';
-  if (usernameInput.value === '' && passwordInput.value === '') {
-    usernameWarning.style.display = 'block';
-    passwordWarning.style.display = 'block';
-    return
-  }
-  else if (passwordInput.value === '' && usernameInput.value !== '') {
-    passwordWarning.style.display = 'block';
-    return
-  }
-  else if (passwordInput.value !== '' && usernameInput.value === '') {
-    usernameWarning.style.display === 'block';
-    return
-  }
-  location.href="../index.html"
-}
-
 function togglePassword () {
   if (passwordInput.type === 'password') {
     openEyeImage.style.display = "none";
@@ -36,10 +17,4 @@ function togglePassword () {
     passwordInput.type = 'password';
   }
 }
-
-passwordInput.addEventListener ('keydown', (event)=> {
-  if (event.key === 'Enter') {
-    location.href = "../index.html"
-  }
-});
 
