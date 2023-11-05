8aac33bbd66832e0a8283dcfc333b20212f590f6
ian <bustillomarkian23@gmail.com>
2023-11-05 22:06:29 +0800

    Make login and signup working

--- scripts/sub-pages/login.js
+++ scripts/sub-pages/login.js
@@ -1,7 +1,8 @@
 import { accounts } from "../../data/account.js";
 
+/* HIDE/SHOW PASSWORD SCRIPT */
+
 const PASSWORD_INPUT = document.querySelector ('.js-password');
-const CONFIRM_PASSWORD_INPUT = document.querySelector ('.js-confirm-password');
 const SHOW_PASSWORD_CHECKBOX = document.querySelector ('.js-show-password-checkbox');
 const SHOW_PASSWORD_LABEL = document.querySelector ('.js-show-password-label');
 
@@ -26,3 +27,38 @@ SHOW_PASSWORD_LABEL.addEventListener ('click', ()=> {
     togglePassword ();
   };
 });
+
+
+/* LOGIN SCRIPT */
+
+const EMAIL_ELEMENT = document.querySelector ('.js-email');
+const PASSWORD_ELEMENT = document.querySelector ('.js-password');
+const LOGIN_BUTTON = document.querySelector ('.js-login-button');
+const LOGIN_FORM = document.querySelector ('.js-login-form');
+const ERROR_MESSAGE = document.querySelector ('.js-error-message');
+
+LOGIN_BUTTON.addEventListener ('click', checkValidity);
+
+function checkValidity () {
+  let matchAccount = false;
+  accounts.forEach ((account)=> {
+    if (account.email === EMAIL_ELEMENT.value && account.password === PASSWORD_ELEMENT.value) {
+      matchAccount = true;
+    }
+    else {
+      return
+    }
+  });
+    if (matchAccount === false) {
+      LOGIN_FORM.addEventListener ('submit', ((event)=> {
+        ERROR_MESSAGE.innerText = 'Incorrect email or password.';
+        setTimeout (() => {
+          ERROR_MESSAGE.innerText = '';
+        }, 3000);
+        event.preventDefault ();
+      }));
+    }
+    else {
+      return true
+    }
+};
